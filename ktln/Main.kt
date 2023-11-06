e5cee27db10440ffbac5bb313be4a1090d68aca5
karibay <k.karibays@gmail.com>
2023-11-06 19:48:39 +0600

    Lesson-11 Adds LinkedList Kotlin

---
+++ ktln/Main.kt
@@ -0,0 +1,12 @@
+package ktln
+
+fun main(){
+    val list: LinkedList = LinkedList()
+    list.add(3)
+    list.add(5)
+    list.add(35)
+    list.add(33)
+    list.add(45)
+    list.del(0)
+    println(list)
+}
